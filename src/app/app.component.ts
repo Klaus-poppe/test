1503b626b268a4d2b2dd94fb9ed487cd0fa126d2
Klaus-poppe <vesparadkt@gmail.com>
2020-04-03 15:49:57 +0530

    fixed slow response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { DialogflowService } from './services/dialogflow.service';
 import { Messages } from './model/messages.model';
 
@@ -7,21 +7,17 @@ import { Messages } from './model/messages.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   public message : Messages;
   public messages : Messages[];
   private disease : string;
   private intentId : Number;
   private diseaseId : Number; 
 
-  ngOnInit() {
-  }
-
-
   constructor(private dialogFlowService: DialogflowService) {
     this.message = new Messages('', 'assets/images/user.jpg');
     this.messages = [
-      new Messages('Welcome to chatbot universe', 'assets/images/bot.png', new Date())
+      new Messages('Which disease whould you like to know about?', 'assets/images/bot.png', new Date())
     ];
     this.disease = ""
     this.diseaseId = null
@@ -30,79 +26,67 @@ export class AppComponent {
 
 
 
- userInput(){
+    userInput(){
 
     if(this.message.content == "")
-        return
+    return
 
-  this.message.timestamp = new Date();
-  this.messages.push(this.message);
-  
+    this.message.timestamp = new Date();
+    this.messages.push(this.message);
 
-  this.dialogFlowService.getResponse(this.message.content).subscribe(
+
+    this.dialogFlowService.getResponse(this.message.content).subscribe(
     (res : any)  => {
 
-       if(res.result.fulfillment.speech != "")  
+      if(res.result.fulfillment.speech != "")  
         this.messages.push( new Messages(res.result.fulfillment.speech, 'assets/images/bot.png', res.timestamp));
 
-       if(res.result.fulfillment.messages[1].payload.hasOwnProperty("disease_name")){
-         this.disease = res.result.fulfillment.messages[1].payload.disease_name
-         this.dialogFlowService.getDiseaseId(this.disease).subscribe( 
-          (res : any) =>{
-            this.diseaseId = res.disease_id
-            console.log(this.diseaseId + " " + this.intentId)
+      else if(res.result.fulfillment.messages[1].payload.hasOwnProperty("disease_name")){
+
+        this.disease = res.result.fulfillment.messages[1].payload.disease_name
+
+        this.dialogFlowService.getDiseaseId(this.disease).subscribe( 
+        (res : any) =>{
+          this.diseaseId = res.disease_id
+
+          if(this.intentId != null)
+            this.displayLinks();
+          else
+            this.messages.push( new Messages("What would u like to know about " + this.disease + "?" , 'assets/images/bot.png', new Date()));
+
         })  
+
+      }else{ 
+
         if(this.intentId == null)
-       this.messages.push( new Messages("What would u like to know about " + this.disease + "?" , 'assets/images/bot.png', res.timestamp));
-       console.log(this.diseaseId + " " + this.intentId)
-      } 
-        
-      if (this.intentId != null || res.result.fulfillment.messages[1].payload.hasOwnProperty("intentID")) {
-        console.log(this.diseaseId + " " + this.intentId)
-        if(this.intentId == null)
-        this.intentId = res.result.fulfillment.messages[1].payload.intentID 
+          this.intentId = res.result.fulfillment.messages[1].payload.intentID 
+
         if(this.diseaseId == null){
-        this.messages.push( new Messages("For which disease?", 'assets/images/bot.png', res.timestamp));
-        console.log(this.diseaseId + " " + this.intentId)
+          this.messages.push( new Messages("For which disease?", 'assets/images/bot.png', res.timestamp));
           return
         }
-         
-        this.dialogFlowService.getLinks(this.diseaseId , this.intentId).subscribe( 
-          (res : any) =>{
-            for (const link of res.links) {
-              this.messages.push( new Messages(link.link, 'assets/images/bot.png', new Date()));
-            }
-        })  
-     
-       this.intentId = null
-       }
-      });
+
+        this.displayLinks();
+      }
+    });
 
 
-  //  if(this.disease != ""){
-  //    this.dialogFlowServic*e.getDiseaseId(this.disease).subscribe( 
-  //      (res : any) =>{
-  //        this.diseaseId = res.diseaseId
-  //        this.messages.push( new Messages("What would u like to know about" + this.disease , 'assets/images/bot.png', res.timestamp));
-       
-  //      })  
-  //   this.disease = ""
-  //   } 
+    this.message = new Messages('', 'assets/images/user.jpg');
+    }
 
-    // if(this.intentId != null){
-    //   if(this.diseaseId == null)
-    //   this.messages.push( new Messages("For which disease?", 'assets/images/bot.png', res.timestamp));
-       
-    //   this.dialogFlowService.getLinks(this.diseaseId , this.intentId).subscribe( 
-    //     (res : any) =>{
-    //       for (const link of res.links) {
-    //         this.messages.push( new Messages(link.link, 'assets/images/bot.png', new Date()));
-    //       }
-    //     })  
-    //  this.intentId = null
-    //  } 
 
-  this.message = new Messages('', 'assets/images/user.jpg');
-}
+    displayLinks() : void{
 
+      this.dialogFlowService.getLinks(this.diseaseId , this.intentId).subscribe( 
+        (res : any) =>{
+          this.messages.push( new Messages("Here are some useful links", 'assets/images/bot.png', new Date()));
+          for (const link of res.links) {
+            this.messages.push( new Messages(link.link, 'assets/images/bot.png', new Date()));
+          }
+      })  
+
+    this.intentId = null
+    }    
+
+    
 }
